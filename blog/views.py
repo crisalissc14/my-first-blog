f51c342bc1899f98985ece0358551b84537e2364
crisalissc14 <cris_c1004@hotmail.com>
2024-10-05 16:58:20 -0500

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Post
 
+#create
 def post_list(request):
-    posts = Post.objects.all()  # Obtiene todas las publicaciones
-    return render(request, 'blog/post_list.html', {'posts': posts})  # Renderiza la plantilla
+    return render(request, 'blog/post_list.html', {})
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Asegúrate de incluir 'include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # Asegúrate de que esto esté aquí
 ]
+
